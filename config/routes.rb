efd22907e80743bd3e15fdb6560ffe517dd4369e
Alperen Bozkurt <mail@alperenbozkurt.net>
2020-05-12 15:54:03 +0000

    Chat styleleri eklendi

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   
   root to: 'chatrooms#index'
-  resources :chatrooms
+  resources :chatrooms do 
+    resource :chatroom_users
+    resources :messages
+  end
 end
